02a6e30e4a2b19582387d85953dec6797d095384
IgorDevFullstack <igormtba@gmail.com>
2025-09-12 13:28:05 -0300

    important notice

--- src/components/Login/LoginMain/SignIn.jsx
+++ src/components/Login/LoginMain/SignIn.jsx
@@ -47,8 +47,8 @@ const SignIn = ({ handleResponse }) => {
                 <Toast.Body>Use this account to sign in as a admin <br />
                     <hr />
                     <div className='bg-dark text-white p-2 px-3 rounded'>
-                        email : Admin@gmail.com <br />
-                        password : 1234 <br />
+                        email : crie em sigup <br />
+                        password : crie em sigup<br />
                     </div>
                     <hr />
                     <div className='bg-primary p-2 rounded text-white'>
